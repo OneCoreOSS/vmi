3913f39a1e33eefefb5c98f33cf2a597cf870b9b
theduck32 <147930445+theduck32@users.noreply.github.com>
2025-06-16 18:07:39 -0400

    [Server] Support new storage interfaces

--- src/server/core/cli/main.js
+++ src/server/core/cli/main.js
@@ -15,9 +15,27 @@ function LoadConfig(FilePath) {
 	return JSON.parse(fs.readFileSync(FilePath, 'utf8'));
 }
 
+const VMStorageList = {
+	"virtio": [],
+	"ide": [],
+	"nvme": [],
+	"scsi": [],
+	"sd": []
+}
+
+const OnlyOne = {
+	"scsipci": "-device virtio-scsi-pci,id=scsi",
+}
+
 /* this basically builds a CLI qemu-system-X command from our vmi config */
 
 function BuildCommand(VMCfg) {
+	VMStorageList.virtio = [];
+	VMStorageList.ide = [];
+	VMStorageList.nvme = [];
+	VMStorageList.scsi = [];
+	VMStorageList.sd = [];
+
 	const UserArch = VMCfg.Base.Arch;
 	const VMArch = ArchMap[UserArch];
 	if (!VMArch) {
@@ -54,12 +72,25 @@ function BuildCommand(VMCfg) {
 	/* Storage CFG */
 	Object.values(VMCfg.Storage || {}).forEach(([type, iface, file]) => {
 		if (type === 'Disk') {
-			args.push(`-drive if=${iface},file="${file}"`);
+			if(iface == "virtio") {
+				args.push(`-drive if=${iface},file="${file}",id=vmi.virtio${VMStorageList[iface].length}`)
+			} else if(iface == "ide") {
+				args.push(`-drive if=${iface},file="${file}",id=vmi.ide${VMStorageList[iface].length}`)
+			} else if(iface == "nvme") {
+				args.push(`-drive if=none,file="${file}",id=vmi.nvme${VMStorageList[iface].length} -device nvme,serial=0x073C0530,drive=vmi.nvme${VMStorageList[iface].length}`)
+			} else if(iface == "scsi") {
+				args.push(`-drive if=none,file="${file}",id=vmi.scsi${VMStorageList[iface].length} ${OnlyOne.scsipci} -device scsi-hd,drive=vmi.scsi${VMStorageList[iface].length}`)
+				OnlyOne.scsipci = "";
+			} else if(iface == "sd") {
+				args.push(`-drive if=none,file="${file}",id=vmi.sdcard${VMStorageList[iface].length} -device sdhci-pci,id=sdcard${VMStorageList[iface].length} -device sd-card,drive=vmi.sdcard${VMStorageList[iface].length}`)
+			}
+			//args.push(`-drive if=${iface},file="${file}"`);
 		} else if (type === 'Floppy') {
 			args.push(`-drive if=floppy,file=${file}`);
 		} else if (type === 'CDROM' || iface === 'iso') {
 			args.push(`-cdrom ${file}`);
 		}
+		VMStorageList[iface].push(0); // Increment drive ID by 1
 	});
 
 	/* Extra CFG */
